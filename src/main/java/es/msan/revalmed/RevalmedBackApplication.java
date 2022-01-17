39eb609b9c6bf943419b93c664e80f03e96076d7
mjcadsan <96058974+mjcadsan@users.noreply.github.com>
2022-01-17 10:56:16 +0100

    Add files via upload

---
+++ src/main/java/es/msan/revalmed/RevalmedBackApplication.java
@@ -0,0 +1,13 @@
+package es.msan.revalmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevalmedBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevalmedBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/msan/revalmed/RevalmedBackApplicationTests.java
@@ -0,0 +1,13 @@
+package es.msan.revalmed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevalmedBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
